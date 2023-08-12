f68a9784e0491780252a123e43b35556146164bc
jakob.bauer <jakobbauer@thesiestagroup.com>
2023-08-12 21:04:42 +0200

    fiddled around with D3 chart

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
+import { PieChartComponent } from './charts/pie-chart/pie-chart.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    HttpClientModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [AppComponent],
+	bootstrap: [AppComponent],
+	imports: [BrowserModule, AppRoutingModule, HttpClientModule, PieChartComponent],
 })
-export class AppModule { }
+export class AppModule {}

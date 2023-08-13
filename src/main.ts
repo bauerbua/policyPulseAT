8bdd329870adfb11700f0c7520e9f25e61aad9b9
jakob.bauer <jakobbauer@thesiestagroup.com>
2023-08-13 07:45:59 +0200

    removed unused imports

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,9 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
-
 import { importProvidersFrom } from '@angular/core';
 import { AppComponent } from './app/app.component';
 import { withInterceptorsFromDi, provideHttpClient } from '@angular/common/http';
 import { AppRoutingModule } from './app/app-routing.module';
 import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
 
-
 bootstrapApplication(AppComponent, {
-    providers: [
-        importProvidersFrom(BrowserModule, AppRoutingModule),
-        provideHttpClient(withInterceptorsFromDi())
-    ]
-})
-  .catch(err => console.error(err));
+	providers: [importProvidersFrom(BrowserModule, AppRoutingModule), provideHttpClient(withInterceptorsFromDi())],
+}).catch(err => console.error(err));
